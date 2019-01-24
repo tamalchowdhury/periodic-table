b119c43e98aa5e5e09763f6e7b000b1cea94dd42
Tamal Anwar <tamalanwaronline@gmail.com>
2019-01-24 20:18:12 +0600

    Github pages

---
+++ precache-manifest.ca33c63a1a4a83b1d21f552fa2026c23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "917c0887cf36b7366811",
+    "url": "/static/js/main.917c0887.chunk.js"
+  },
+  {
+    "revision": "ec5d15567dc43003ccaa",
+    "url": "/static/js/1.ec5d1556.chunk.js"
+  },
+  {
+    "revision": "917c0887cf36b7366811",
+    "url": "/static/css/main.5a3437e5.chunk.css"
+  },
+  {
+    "revision": "003b3f8f103e9f5cb4400a5f0c8a7627",
+    "url": "/index.html"
+  }
+];
