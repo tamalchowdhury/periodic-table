ba893b6d74073a45be2280078dfc799637f967e6
tamalweb <tamalweb@protonmail.com>
2021-06-28 22:52:30 +0600

    Updates

---
+++ precache-manifest.cc1eeb684eace0c76ad3d9604b45a3f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2685d36f1a308b78eedf",
+    "url": "/periodic-table/static/css/main.b246b9eb.chunk.css"
+  },
+  {
+    "revision": "2685d36f1a308b78eedf",
+    "url": "/periodic-table/static/js/main.2685d36f.chunk.js"
+  },
+  {
+    "revision": "ec5d15567dc43003ccaa",
+    "url": "/periodic-table/static/js/1.ec5d1556.chunk.js"
+  },
+  {
+    "revision": "8e10912f746c7819dd31",
+    "url": "/periodic-table/static/js/runtime~main.8e10912f.js"
+  },
+  {
+    "revision": "54a015c1d276365c14af4cc447347a4f",
+    "url": "/periodic-table/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/periodic-table/precache-manifest.3b840815bae1bfc9ae06291bac41e691.js"
+  "/periodic-table/precache-manifest.cc1eeb684eace0c76ad3d9604b45a3f7.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/periodic-table/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
