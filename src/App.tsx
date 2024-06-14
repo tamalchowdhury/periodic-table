48ff724d1c2883675e9a1ef1d1ba3bec848bb77c
Tamal Anwar Chowdhury <tamalanwaronline@gmail.com>
2024-06-14 21:29:38 +0600

    refactor: start refactoring to show the table to screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,110 @@
+import { useState } from "react"
 import Element from "./Element"
 import "./styles/main.css"
 
 export default function App() {
+  const [showInfo, setShowInfo] = useState(false)
+  const [element, setElement] = useState({})
+
+  const handleshowInfo = (num) => {
+    this.setState({ showInfo: true, element: elements[num] })
+  }
+
+  const handlecloseInfo = () => {
+    this.setState({ showInfo: false })
+  }
+
+  const populateElements = (start, end) => {
+    let items = []
+    for (let i = start; i <= end; i++) {
+      items.push(<Element showInfo={showInfo} num={i} />)
+    }
+    return items
+  }
+
+  const {
+    name,
+    summary,
+    symbol,
+    category,
+    number,
+    source,
+    appearance,
+    atomic_mass,
+    molar_heat,
+    density,
+    melt,
+    boil,
+  } = element
+
   return (
     <>
-      hello
-      <Element num={5} showInfo={() => {}} />
+      <div className='wrapper'>
+        <div id='table'>
+          {/* Elements 1-4 */}
+          {populateElements(1, 4)}
+          {/* Information Table */}
+          {showInfo && (
+            <>
+              <div id='element-box' className={`${category}`}>
+                <div className='number'>{number}</div>
+                <div className='symbol'>{symbol}</div>
+                <div className='element-name'>{name}</div>
+              </div>
+              <div id='information'>
+                <div
+                  onClick={handlecloseInfo}
+                  className='close-button'
+                  title='Close Info'
+                >
+                  Close [&times;]
+                </div>
+                <div>
+                  <h1 className='big_title'>{name}</h1>
+                  <span className={`cat_name ${category}`}>{category}</span>
+                  {appearance && (
+                    <div className='appearance'>
+                      <strong>Appearance:</strong> {appearance}
+                    </div>
+                  )}
+                  <div className='atom_info'>
+                    <span>Atomic Mass: {atomic_mass} | </span>
+                    <span>Density: {density}</span>
+                    {molar_heat && <span> | Molar Heat: {molar_heat}</span>}
+                    {melt && <span> | Melt: {melt}K</span>}
+                    {boil && <span> | Boil: {boil}K</span>}
+                  </div>
+                  <div>
+                    {summary} ...{" "}
+                    <a target='_blank' href={source}>
+                      Source
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </>
+          )}
+          {/* Populating elements from 5-57 */}
+          {populateElements(5, 57)}
+          {/* Lanthanoids split 72-89 */}
+          {populateElements(72, 89)}
+          {/* Actinoids split 104-119*/}
+          {populateElements(104, 118)}
+          {/* Lanthenoids 58-71*/}
+          {populateElements(58, 71)}
+          {/* Actionoids 90-103 */}
+          {populateElements(90, 103)}
+        </div>
+        <footer className='footer center'>
+          <span>
+            2024, Put together by{" "}
+            <a href='https://blogkori.com' target='_blank' rel='noopener'>
+              Tamal Chowdhury
+            </a>
+          </span>
+          GitHub Button
+        </footer>
+      </div>
     </>
   )
 }
