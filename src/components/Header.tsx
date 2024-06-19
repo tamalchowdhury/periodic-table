3f237058e4d4c3fb6393e88196120d1854beb8d3
Tamal Anwar Chowdhury <tamalanwaronline@gmail.com>
2024-06-19 21:47:09 +0600

    chore: reset <a> tag in css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,9 @@ export default function Header() {
   return (
     <header className='header'>
       <div className='wrap'>
-        <h1>The Periodic Table</h1>
+        <h1>
+          <a href='/'>The Periodic Table</a>
+        </h1>
       </div>
     </header>
   )
