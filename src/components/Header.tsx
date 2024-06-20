16f1acdcaf70240a5c83307aa0de9e85004bd788
Tamal Anwar Chowdhury <tamalanwaronline@gmail.com>
2024-06-20 22:03:06 +0600

    chore: add css styles for the header links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,27 @@
-import { SITE_TITLE } from "../lib/consts"
+import { GITHUB_REPO, SITE_TITLE } from "../lib/consts"
 import GithubLink from "./GithubLink"
 
 export default function Header() {
   return (
     <header className='header'>
       <div className='wrap header__wrap'>
-        <h1>
+        <h1 className='header__logo'>
           <a href='/'>{SITE_TITLE}</a>
         </h1>
-        <span>
-          <GithubLink />
+        <span className='header__text u-flex'>
+          <a href={GITHUB_REPO} target='_blank'>
+            Star on Github
+          </a>{" "}
+          🌟 <GithubLink />
         </span>
-        <span>
+        <span className='header__text u-flex'>
           <a
-            style={{ textDecoration: "underline" }}
             href='https://www.blogkori.com/periodic-table-react'
             target='_blank'
           >
             How I Built it?
           </a>
         </span>
-        
       </div>
     </header>
   )
